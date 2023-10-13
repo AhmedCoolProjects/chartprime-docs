83eae4ec453b246a062ae8255409a605a8eb30b3
Ahmed BARGADY <bargadyahmed2000@gmail.com>
2023-10-13 23:23:32 +0100

    ignore broken links for now

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,8 +21,8 @@ const config = {
   organizationName: "chartprime", // Usually your GitHub org/user name.
   projectName: "chartprime-docs", // Usually your repo name.
 
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
+  onBrokenLinks: "ignore",
+  onBrokenMarkdownLinks: "ignore",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
